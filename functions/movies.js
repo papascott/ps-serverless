71838b8f42a27ebb31b4610d706a8a9998bc7a5d
papascott <papascott@gmail.com>
2021-01-29 12:57:37 +0100

    Fetch data from OMDb API

--- functions/movies.js
+++ functions/movies.js
@@ -1,6 +1,27 @@
+const { URL } = require('url');
+const fetch = require('node-fetch');
 const movies = require('../data/movies.json');
+require('dotenv').config();
 
-exports.handler = async () => ({
-  statusCode: 200,
-  body: JSON.stringify(movies),
-});
+exports.handler = async () => {
+  const api = new URL('https://www.omdbapi.com/');
+  api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    api.searchParams.set('i', movie.id);
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const scores = data.Ratings;
+        return { ...movie, scores };
+      });
+  });
+
+  // https://www.learnwithjason.dev/blog/keep-async-await-from-blocking-execution/
+  const moviesWithRatings = await Promise.all(promises);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(moviesWithRatings),
+  };
+};
